76cfd8f8a6e3fd6e4f263440ad9d0ad12355c3e0
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-06 20:02:44 -0500

    Changes to be committed: 	modified:   bolsillos digitales/retos_poo/principal/inicio.cpp 	modified:   bolsillos digitales/retos_poo/principal/inicio.exe

--- bolsillos digitales/retos_poo/principal/inicio.cpp
+++ bolsillos digitales/retos_poo/principal/inicio.cpp
@@ -1,11 +1,11 @@
-#include "../salud/persona.h"
+#include "../salud/persona.h"//agregar la clase persona con todos sus metodos y atributos
 
-using namespace std;
 
-int main()
+
+int main()//creamso el main aqui
 {
-    persona persona1 = persona::pedirDatos();
-    persona1.mostrarPersona(persona1);
+    persona persona1 = persona::pedirDatos();//objeto persona1 va a ser lo que se haga en pedir datos
+    persona1.mostrarPersona(persona1);//mostrar persona toma el objeto persona 1 como parametro
 
     return 0;
 }
