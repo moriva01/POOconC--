b57ffbc538e72e142dd5b2c64e5e67ad4dfb37a6
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-06 20:07:24 -0500

    Changes to be committed: 	modified:   bolsillos digitales/retos_poo/salud/persona.h

--- bolsillos digitales/retos_poo/salud/persona.h
+++ bolsillos digitales/retos_poo/salud/persona.h
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream>//uso de cin y cout
 
-using namespace std;
+using namespace std;//espacio std para poder ingresar datos
 
-class persona
+class persona//clase persona
 {
 
-private:
+private://atributos que pedia el ejercicio
     string tipo_doc;
     long long documento;
     string nombre;
@@ -16,13 +16,13 @@ private:
     bool sexo;
 
 public:
-    persona() {}
+    persona() {}//constructor vacio de los objetos
 
-    static persona pedirDatos()
+    static persona pedirDatos()//metodo que pide datos
     {
-        persona persona;
+        persona persona;//inicializa objeto auxiliar
         cout << "tipo de documento: ";
-        cin >> persona.tipo_doc;
+        cin >> persona.tipo_doc;//pide los datos
         cout << "numero de documento: ";
         cin >> persona.documento;
         cout << "nombre: ";
@@ -40,15 +40,15 @@ public:
              << "--1 para masculino ";
         cin >> persona.sexo;
 
-        return persona;
+        return persona;//retorna el objeto
     }
 
-    void calcularmc(float peso, float estatura)
+    void calcularmc(float peso, float estatura)//calcula masa corporal pasan peso y estatura como parametros
     {
 
-        float peso_final = peso / (estatura * estatura);
+        float peso_final = peso / (estatura * estatura);//calcula masa corporal
 
-        if (peso_final < 20)
+        if (peso_final < 20)//hace el analisis del peso
         {
             cout << "peso debajo de lo ideal" << endl;
         }
@@ -62,10 +62,10 @@ public:
         }
     }
 
-    void mayorEdad(int edad)
+    void mayorEdad(int edad)//metodo saber si es mayor de edad entra edad como parametro
     {
 
-        if (edad >= 18)
+        if (edad >= 18)//revias sea mayor de edad
         {
 
             cout << "--- mayor de edad" << endl;
@@ -77,10 +77,10 @@ public:
         }
     }
 
-    void mostrarPersona(persona aux)
+    void mostrarPersona(persona aux)//metodo mostrar persona, entra objeto persona como parametro
     {
 
-        cout << "-------------------------------------------------- " << endl;
+        cout << "-------------------------------------------------- " << endl;//imprime todo eso
         cout << "------------- tipo de documento: " << aux.tipo_doc << endl;
         cout << "------------- numero de documento: " << aux.documento << endl;
         cout << "------------- nombre: " << aux.nombre << endl;
@@ -89,7 +89,7 @@ public:
         cout << "------------- estatura (m) " << aux.estatura << endl;
         cout << "------------- edad: " << aux.edad << endl;
 
-        if (aux.sexo == 0)
+        if (aux.sexo == 0)//revisa si es masculino o femenino
         {
             cout << "------------- sexo: Femenino " << endl;
         }
@@ -102,14 +102,14 @@ public:
         cout << "-------------------------------------------------- " << endl;
         cout << "------------- segun su masa corporal:" << endl;
 
-        aux.calcularmc(aux.peso, aux.estatura);
+        aux.calcularmc(aux.peso, aux.estatura);//instancia el metodo de masa corporal
 
         cout << "------------- segun su edad:" << endl;
-        aux.mayorEdad(aux.edad);
+        aux.mayorEdad(aux.edad);//instancia metodo de la mayoria de edad
     }
 };
 
-/*int main()
+/*int main()//este era el main pero se paso para la clase del otro paquete como decia el punto 2
 {
 
     persona persona1 = persona::pedirDatos();
