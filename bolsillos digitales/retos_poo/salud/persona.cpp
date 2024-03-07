e23571a219bbe5868cd0ecf5fa525e3f35377e0e
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-06 19:40:05 -0500

    Changes to be committed: 	modified:   bolsillos digitales/retos_poo/salud/persona.cpp 	modified:   bolsillos digitales/retos_poo/salud/persona.exe

--- bolsillos digitales/retos_poo/salud/persona.cpp
+++ bolsillos digitales/retos_poo/salud/persona.cpp
@@ -18,7 +18,7 @@ private:
 public:
     persona() {}
 
-    static persona ingresar_persona()
+    static persona pedirDatos()
     {
         persona persona;
         cout << "tipo de documento: ";
@@ -31,21 +31,88 @@ public:
         cin >> persona.apellido;
         cout << "peso (kg): ";
         cin >> persona.peso;
-        cout << "estatura (cm): ";
+        cout << "estatura (m): ";
         cin >> persona.estatura;
         cout << "edad: ";
         cin >> persona.edad;
-        cout << "sexo: --0 para femenino"<<endl<<"--1 para masculino): ";
+        cout << "sexo" << endl
+             << " --0 para femenino" << endl
+             << "--1 para masculino ";
         cin >> persona.sexo;
 
         return persona;
+    }
+
+    void calcularmc(float peso, float estatura)
+    {
+
+        float peso_final = peso / (estatura * estatura);
+
+        if (peso_final < 20)
+        {
+            cout << "peso debajo de lo ideal" << endl;
+        }
+        else if (peso_final <= 25 && peso_final > 20)
+        {
+            cout << "peso es ideal" << endl;
+        }
+        else
+        {
+            cout << "sobre peso" << endl;
+        }
+    }
+
+    void mayorEdad(int edad)
+    {
+
+        if (edad >= 18)
+        {
+
+            cout << "--- mayor de edad" << endl;
+        }
+        else
+        {
+
+            cout << "--- menor de edad" << endl;
+        }
+    }
+
+    void mostrarPersona(persona aux)
+    {
+
+        cout << "-------------------------------------------------- " << endl;
+        cout << "------------- tipo de documento: " << aux.tipo_doc << endl;
+        cout << "------------- numero de documento: " << aux.documento << endl;
+        cout << "------------- nombre: " << aux.nombre << endl;
+        cout << "------------- apellido: " << aux.apellido << endl;
+        cout << "------------- peso (kg): " << aux.peso << endl;
+        cout << "------------- estatura (m) " << aux.estatura << endl;
+        cout << "------------- edad: " << aux.edad << endl;
+
+        if (aux.sexo == 0)
+        {
+            cout << "------------- sexo: Femenino " << endl;
+        }
+        else
+        {
+
+            cout << "------------- sexo: Masculino " << endl;
+        }
+
+        cout << "-------------------------------------------------- " << endl;
+        cout << "segun su masa corporal:" << endl;
+
+        aux.calcularmc(aux.peso, aux.estatura);
+        aux.mayorEdad(aux.edad);
     }
 };
 
 int main()
 {
 
-    persona persona1 = persona::ingresar_persona();
+    persona persona1 = persona::pedirDatos();
+
+    persona1.mostrarPersona(persona1);
 
     return 0;
 }
