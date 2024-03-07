3a96f687626df157ee7afffdc2b561273a99fb2e
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-06 20:22:12 -0500

    Changes to be committed: 	modified:   bolsillos digitales/nequi/modelo/usuario.cpp

--- bolsillos digitales/nequi/modelo/usuario.cpp
+++ bolsillos digitales/nequi/modelo/usuario.cpp
@@ -1,18 +1,35 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-class usuario{
-
+class usuario
+{
 
 private:
-string nombre_usuario;
-string apellido_usuario;
-string correo_usuario;
-int tipo_documento;
-int long long numero_documento;
-int long long numero_celular;
-int contraseña;
+    string nombre_usuario;
+    string apellido_usuario;
+    string correo_usuario;
+    int tipo_documento;
+    int long long numero_documento;
+    int long long numero_celular;
+    int contrasena;
 
+public:
+    usuario(string nombre_usuario,
+            string apellido_usuario,
+            string correo_usuario,
+            int tipo_documento,
+            int long long numero_documento,
+            int long long numero_celular,
+            int contrasena)
+    {
 
+        this->nombre_usuario = nombre_usuario;
+        this->apellido_usuario = apellido_usuario;
+        this->correo_usuario = correo_usuario;
+        this->tipo_documento = tipo_documento;
+        this->numero_documento = numero_documento;
+        this->numero_celular = numero_celular;
+        this->contrasena = contrasena;
+    }
 };
