e5cfac190d3998c45565391c20f76e6c5e27c812
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-06 20:16:52 -0500

    Changes to be committed: 	deleted:    bolsillos digitales/nequi/modelo/persona.cpp 	modified:   bolsillos digitales/nequi/modelo/usuario.cpp

---
+++ bolsillos digitales/nequi/modelo/usuario.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+class usuario{
+
+
+private:
+string nombre_usuario;
+string apellido_usuario;
+string correo_usuario;
+int tipo_documento;
+int long long numero_documento;
+int long long numero_celular;
+int contraseña;
+
+
+};
