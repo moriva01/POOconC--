4a63b3e3b6504468a06fa795633418f0777c297f
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-08 13:45:24 -0500

    setters y getters

--- bolsillos digitales/retos_poo/salud/persona.h
+++ bolsillos digitales/retos_poo/salud/persona.h
@@ -1,16 +1,14 @@
 //                                                           ivan david moreno vargas
 
+// cambiamos a .h para poder importar en la otra clase
+#include <iostream> //uso de cin y cout
 
+using namespace std; // espacio std para poder ingresar datos
 
-//cambiamos a .h para poder importar en la otra clase
-#include <iostream>//uso de cin y cout
-
-using namespace std;//espacio std para poder ingresar datos
-
-class persona//clase persona
+class persona // clase persona
 {
 
-private://atributos que pedia el ejercicio
+private: // atributos que pedia el ejercicio
     string tipo_doc;
     long long documento;
     string nombre;
@@ -21,13 +19,97 @@ private://atributos que pedia el ejercicio
     bool sexo;
 
 public:
-    persona() {}//constructor vacio de los objetos
+    persona() {} // constructor vacio de los objetos
 
-    static persona pedirDatos()//metodo que pide datos
+    //--------------------------------------------------------------- getters
+
+    string getTipoDoc() const
     {
-        persona persona;//inicializa objeto auxiliar
+        return tipo_doc;
+    }
+
+    long long getDocumento() const
+    {
+        return documento;
+    }
+
+    string getNombre() const
+    {
+        return nombre;
+    }
+
+    string getApellido() const
+    {
+        return apellido;
+    }
+
+    float getPeso() const
+    {
+        return peso;
+    }
+
+    float getEstatura() const
+    {
+        return estatura;
+    }
+
+    int getEdad() const
+    {
+        return edad;
+    }
+
+    bool getSexo() const
+    {
+        return sexo;
+    }
+
+    //----------------------------------------------------------------- setters
+
+    void setTipoDoc(const string &tipo_doc)
+    {
+        this->tipo_doc = tipo_doc;
+    }
+
+    void setDocumento(long long documento)
+    {
+        this->documento = documento;
+    }
+
+    void setNombre(const string &nombre)
+    {
+        this->nombre = nombre;
+    }
+
+    void setApellido(const string &apellido)
+    {
+        this->apellido = apellido;
+    }
+
+    void setPeso(float peso)
+    {
+        this->peso = peso;
+    }
+
+    void setEstatura(float estatura)
+    {
+        this->estatura = estatura;
+    }
+
+    void setEdad(int edad)
+    {
+        this->edad = edad;
+    }
+
+    void setSexo(bool sexo)
+    {
+        this->sexo = sexo;
+    }
+
+    static persona pedirDatos() // metodo que pide datos
+    {
+        persona persona; // inicializa objeto auxiliar
         cout << "tipo de documento: ";
-        cin >> persona.tipo_doc;//pide los datos
+        cin >> persona.tipo_doc; // pide los datos
         cout << "numero de documento: ";
         cin >> persona.documento;
         cout << "nombre: ";
@@ -45,15 +127,15 @@ public:
              << "--1 para masculino ";
         cin >> persona.sexo;
 
-        return persona;//retorna el objeto
+        return persona; // retorna el objeto
     }
 
-    void calcularmc(float peso, float estatura)//calcula masa corporal pasan peso y estatura como parametros
+    void calcularmc(float peso, float estatura) // calcula masa corporal pasan peso y estatura como parametros
     {
 
-        float peso_final = peso / (estatura * estatura);//calcula masa corporal
+        float peso_final = peso / (estatura * estatura); // calcula masa corporal
 
-        if (peso_final < 20)//hace el analisis del peso
+        if (peso_final < 20) // hace el analisis del peso
         {
             cout << "peso debajo de lo ideal" << endl;
         }
@@ -67,10 +149,10 @@ public:
         }
     }
 
-    void mayorEdad(int edad)//metodo saber si es mayor de edad entra edad como parametro
+    void mayorEdad(int edad) // metodo saber si es mayor de edad entra edad como parametro
     {
 
-        if (edad >= 18)//revias sea mayor de edad
+        if (edad >= 18) // revias sea mayor de edad
         {
 
             cout << "--- mayor de edad" << endl;
@@ -82,10 +164,10 @@ public:
         }
     }
 
-    void mostrarPersona(persona aux)//metodo mostrar persona, entra objeto persona como parametro
+    void mostrarPersona(persona aux) // metodo mostrar persona, entra objeto persona como parametro
     {
 
-        cout << "-------------------------------------------------- " << endl;//imprime todo eso
+        cout << "-------------------------------------------------- " << endl; // imprime todo eso
         cout << "------------- tipo de documento: " << aux.tipo_doc << endl;
         cout << "------------- numero de documento: " << aux.documento << endl;
         cout << "------------- nombre: " << aux.nombre << endl;
@@ -94,7 +176,7 @@ public:
         cout << "------------- estatura (m) " << aux.estatura << endl;
         cout << "------------- edad: " << aux.edad << endl;
 
-        if (aux.sexo == 0)//revisa si es masculino o femenino
+        if (aux.sexo == 0) // revisa si es masculino o femenino
         {
             cout << "------------- sexo: Femenino " << endl;
         }
@@ -107,10 +189,10 @@ public:
         cout << "-------------------------------------------------- " << endl;
         cout << "------------- segun su masa corporal:" << endl;
 
-        aux.calcularmc(aux.peso, aux.estatura);//instancia el metodo de masa corporal
+        aux.calcularmc(aux.peso, aux.estatura); // instancia el metodo de masa corporal
 
         cout << "------------- segun su edad:" << endl;
-        aux.mayorEdad(aux.edad);//instancia metodo de la mayoria de edad
+        aux.mayorEdad(aux.edad); // instancia metodo de la mayoria de edad
     }
 };
 
