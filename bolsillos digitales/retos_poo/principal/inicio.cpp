1f5e9c9d5856e87f2eda1ad90b39283dcfb4bc24
moriva01 <ivanda.moreno@urosario.edu.co>
2024-03-10 11:20:48 -0500

    Changes to be committed: 	modified:   bolsillos digitales/retos_poo/principal/inicio.cpp 	modified:   bolsillos digitales/retos_poo/salud/empleado.h

--- bolsillos digitales/retos_poo/principal/inicio.cpp
+++ bolsillos digitales/retos_poo/principal/inicio.cpp
@@ -1,29 +1,47 @@
 //                                                           ivan david moreno vargas
 
+#include "../salud/persona.h"  //agregar la clase persona con todos sus metodos y atributos
+#include "../salud/empleado.h" //agregar la clase empleado con todos sus metodos y atributos
 
-#include "../salud/persona.h"//agregar la clase persona con todos sus metodos y atributos
-
-
-
-int main()//creamso el main aqui
+int main() // creamso el main aqui
 {
-    persona persona1 = persona::pedirDatos();//objeto persona1 va a ser lo que se haga en pedir datos
-    persona1.mostrarPersona(persona1);//mostrar persona toma el objeto persona 1 como parametro
+    persona persona1 = persona::pedirDatos(); // objeto persona1 va a ser lo que se haga en pedir datos
+   // empleado empleado1("cc",987654321,"juan","perez",45.0,1.50,25,false,"operario",8000,36,"IT");
+    cout << "------------------------------------------------------------------------------------- datos persona:" << endl;
+    persona1.mostrarPersona(persona1); // mostrar persona toma el objeto persona 1 como parametro
 
-    static float peso_final=persona1.calcularmc(persona1.getPeso(),persona1.getEstatura());//toma el valor de retorno del metodo calcularmc
+    static float peso_final = persona1.calcularmc(persona1.getPeso(), persona1.getEstatura()); // toma el valor de retorno del metodo calcularmc
 
     if (peso_final < 20) // hace el analisis del peso
-        {
-            cout << "peso bajo" << endl;
-        }
-        else if (peso_final <= 25 && peso_final > 20)
-        {
-            cout << "peso ideal" << endl;
-        }
-        else
-        {
-            cout << "sobrepeso" << endl;
-        }
+    {
+        cout << "peso bajo" << endl;
+    }
+    else if (peso_final <= 25 && peso_final > 20)
+    {
+        cout << "peso ideal" << endl;
+    }
+    else
+    {
+        cout << "sobrepeso" << endl;
+    }
+    cout<<"-------------------------------------------------------------------------------------------------------"<<endl;
+
+    empleado empleado1;
+    empleado1.setTipoDoc("cc");
+    empleado1.setDocumento(987654321);
+    empleado1.setNombre("juan andres");
+    empleado1.setApellido("perez gomez");
+    empleado1.setCargo("operario");
+    empleado1.setHorasTrabajadas(36);
+    empleado1.setValorHora(8500);
+
+    cout<<"empelado -----------------------"<<endl;
+    cout<<"-- "<<empleado1.getTipoDoc()<<"       - "<<empleado1.getDocumento()<<endl;
+    cout<<"-- Nombre: "<<empleado1.getNombre()<<" "<<empleado1.getApellido()<<endl;
+    cout<<"-- Cargo: "<<empleado1.getCargo()<<"     -- Horas trabajadas: "<<empleado1.getHorasTrabajadas()<<"    --Valor por hora: "<<empleado1.getValorHora()<<endl;
+    cout<<"-- Total a pagar: "<<empleado1.calcularHonorarios()<<endl;
+
+
 
     return 0;
 }
--- bolsillos digitales/retos_poo/salud/empleado.h
+++ bolsillos digitales/retos_poo/salud/empleado.h
@@ -1,6 +1,68 @@
-class empleado{
+#include "persona.h"
+
+class empleado : public persona
+{
+
+private:
+    string cargo;
+    float valor_hora;
+    int horas_trabajadas;
+    string departamento;
+
+public:
+    empleado() {}
+
+    // ------------------------------------------------------------------------------- Setters
+    void setCargo(string c)
+    {
+        cargo = c;
+    }
+
+    void setValorHora(float v)
+    {
+        valor_hora = v;
+    }
+
+    void setHorasTrabajadas(int h)
+    {
+        horas_trabajadas = h;
+    }
+
+    void setDepartamento(string d)
+    {
+        departamento = d;
+    }
+
+    // --------------------------------------------------- Getters
+    string getCargo()
+    {
+        return cargo;
+    }
+
+    float getValorHora()
+    {
+        return valor_hora;
+    }
+
+    int getHorasTrabajadas()
+    {
+        return horas_trabajadas;
+    }
+
+    string getDepartamento()
+    {
+        return departamento;
+    }
+
+    //----------------------------------------------------- Metodos
+
+    float calcularHonorarios(){
 
 
+        float aux= (this->horas_trabajadas*this->valor_hora);
+        float reteica=(aux*0.966)/100;
+        aux-=reteica;
 
-    
+        return aux;
+    }
 };
